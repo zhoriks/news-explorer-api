87e52f04068e765e7130eb2913929dc8fdc6ad4d
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-07 18:40:37 +0500

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('./mongod');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('./middlewares/cors');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index.js');
@@ -12,6 +13,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
---
+++ middlewares/cors.js
@@ -0,0 +1,26 @@
+const corsControl = require('express').Router();
+
+// Массив разешённых доменов
+const allowedCors = [
+  'https://www.api.news-explorer.online',
+  'http://www.api.news-explorer.online',
+  'https://api.news-explorer.online',
+  'https://api.news-explorer.online',
+  'http://localhost:8080',
+];
+
+corsControl.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
+  );
+  next();
+});
+
+module.exports = corsControl;
