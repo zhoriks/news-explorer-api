b3524e2604e3c46c80406001c7e6a527a538a6a8
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-07 19:19:41 +0500

    add cookie

--- controllers/users.js
+++ controllers/users.js
@@ -25,11 +25,13 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       if (!user) {
         throw new AuthorisationError('Ошибка авторизации');
       }
-      res.send({
-        token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' }),
+      res.cookie('jwt', token, {
+        maxAge: 1000 * 60 * 60 * 7,
+        httpOnly: true,
       });
     })
     .catch(next);
