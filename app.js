cf9f5e836d55e695ab2eb2a79c2cffbdbd7b2532
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-08 23:53:14 +0500

    Edit cors

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const { notFoundMessage } = require('./shared/messages');
 
 const app = express();
 
-app.use(cors);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(rateLimiter);
 app.use(helmet());
 app.use(cookieParser());
