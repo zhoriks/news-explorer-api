e408c7f0457ffc8fa684f1b5c3a40a0822b05cd2
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-08 23:30:50 +0500

    Edit cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,11 @@
 const { allowedDomains } = require('../config');
 
 const cors = (req, res, next) => {
-  if (req.headers.origin && allowedDomains.includes(req.headers.origin)) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  } else {
-    res.header('Access-Control-Allow-Origin', 'https://news-explorer.online');
+  const { origin } = req.headers;
+  if (allowedDomains.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
   next();
 };
 
