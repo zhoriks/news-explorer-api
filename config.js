94a4bf43d61ec946ae1cddc7e51ac7e66ada2663
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-08 23:36:57 +0500

    Edit cors

--- config.js
+++ config.js
@@ -3,11 +3,4 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   jwtSecret: isDev ? 'big-secret-for-a-small-company' : process.env.JWT_SECRET,
   appPort: isDev ? 3000 : process.env.PORT,
-  allowedDomains: [
-    'https://www.news-explorer.online',
-    'http://www.news-explorer.online',
-    'https://news-explorer.online',
-    'https://news-explorer.online',
-    'http://localhost:8080',
-  ],
 };
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,11 @@
-const { allowedDomains } = require('../config');
+const allowedDomains = [
+  'https://www.news-explorer.online',
+  'http://www.news-explorer.online',
+  'https://news-explorer.online',
+  'https://news-explorer.online',
+  'http://localhost:8080',
+];
+
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
