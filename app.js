938db9003299648e16065bf6a270514e233e23f1
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-03-09 14:54:27 +0500

    Edit cors

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ const { errors } = require('celebrate');
 const { appPort } = require('./config');
 const NotFoundError = require('./errors/not-found-err');
 const errorHandler = require('./middlewares/error-handler');
+const rateLimiter = require('./middlewares/rate-limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
 const { notFoundMessage } = require('./shared/messages');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use(rateLimiter);
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
